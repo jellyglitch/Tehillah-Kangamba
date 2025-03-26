80010f4671f29a5a0de93a29afb5a508f0d4c2b8
Jane Tang <57682395+jellyglitch@users.noreply.github.com>
2025-03-26 12:32:53 -0500

    Close the menu toggle when clicking outside of the menu as well.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+//toggling menu button
+menuToggle.addEventListener("click", () => { 
+  navLinks.classList.toggle("show-menu");
 });
+
+//clicking outside of menu to close menu
+document.addEventListener('click', e => { 
+  if(!navLinks.contains(e.target) && !menuToggle.contains(e.target)){
+      navLinks.classList.remove("show-menu");
+  }
+})
