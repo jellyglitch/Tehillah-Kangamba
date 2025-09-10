347b7d4af7371926adf7887b25d68f8a7db99874
Jane Tang <57682395+jellyglitch@users.noreply.github.com>
2025-09-10 15:45:53 -0500

    Make project page responsive.

--- scripts.js
+++ scripts.js
@@ -13,25 +13,3 @@ document.addEventListener('click', e => {
       navLinks.classList.remove("show-menu");
   }
 })
-
-//////////////// Slider
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-const slides = document.querySelectorAll(".slide");
-let currIndex = 0;
-
-const showSlide = (index) => {
-  slides.forEach((slide, i) => {
-    slide.classList.toggle('active', i === index);
-  });
-};
-
-nextButton.addEventListener("click", () => {
-  currIndex = (currIndex - 1 + slides.length) % slides.length;
-  showSlide(currIndex);
-});
-
-prevButton.addEventListener("click", () => {
-  currIndex = (currIndex + 1) % slides.length;
-  showSlide(currIndex);
-});
